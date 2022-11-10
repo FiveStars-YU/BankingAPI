d1ce86f1533f848b18e981f76e699fed6b8839db
Smasood1 <saadmasood219@gmail.com>
2022-11-10 13:42:42 -0500

    Adding only Account Id attribute

--- src/main/java/com/FiveStarsYU/BankingAPI/models/Account.java
+++ src/main/java/com/FiveStarsYU/BankingAPI/models/Account.java
@@ -1,4 +1,13 @@
 package com.FiveStarsYU.BankingAPI.models;
+import javax.persistence.*;
 
+
+@Entity
 public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+
 }
