575d11b9116298c6b62c9c9b441b09577d972a6e
Smasood1 <saadmasood219@gmail.com>
2022-11-10 15:18:44 -0500

    added Account Class

--- src/main/java/com/FiveStarsYU/BankingAPI/models/Account.java
+++ src/main/java/com/FiveStarsYU/BankingAPI/models/Account.java
@@ -6,11 +6,75 @@ import javax.persistence.*;
 public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
+    @Column(name = "accountId", nullable = false)
     private Long id;
 
     private Enum type;
     private String nickname;
+    private Integer rewards;
+    private Double balance;
+    private Customer customer;
 
+    public Account() {
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Enum getType() {
+        return type;
+    }
+
+    public void setType(Enum type) {
+        this.type = type;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public Integer getRewards() {
+        return rewards;
+    }
+
+    public void setRewards(Integer rewards) {
+        this.rewards = rewards;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", type=" + type +
+                ", nickname='" + nickname + '\'' +
+                ", rewards=" + rewards +
+                ", balance=" + balance +
+                ", customer=" + customer +
+                '}';
+    }
 }
