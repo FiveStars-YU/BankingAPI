cb696f18a081bfc4aef819166e62e3af4d23799e
Smasood1 <saadmasood219@gmail.com>
2022-11-11 19:48:34 -0500

    Account Repo

--- src/main/java/com/FiveStarsYU/BankingAPI/repository/AccountRepo.java
+++ src/main/java/com/FiveStarsYU/BankingAPI/repository/AccountRepo.java
@@ -1,4 +1,10 @@
 package com.FiveStarsYU.BankingAPI.repository;
 
-public interface AccountRepo {
+import com.FiveStarsYU.BankingAPI.models.Account;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepo extends CrudRepository<Account,Long> {
+    Iterable<Account> getAllAccountsByCustomerId(Long customerId);
 }
