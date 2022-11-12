fb89c12e47344750f67eaa2e7a969114421301af
Smasood1 <saadmasood219@gmail.com>
2022-11-11 21:15:59 -0500

    removed verify and repalced with boolean check

--- src/main/java/com/FiveStarsYU/BankingAPI/services/AccountServices.java
+++ src/main/java/com/FiveStarsYU/BankingAPI/services/AccountServices.java
@@ -1,6 +1,7 @@
 package com.FiveStarsYU.BankingAPI.services;
 
 import com.FiveStarsYU.BankingAPI.models.Account;
+import com.FiveStarsYU.BankingAPI.models.Customer;
 import com.FiveStarsYU.BankingAPI.repository.AccountRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,11 +15,20 @@ public class AccountServices {
     @Autowired
     private AccountRepo accountRepo;
 
-    //private CustomerRepo customerRepo;
+    @Autowired
+    private CustomerRepo customerRepo;
 
-    //verify account
-    public void verifyAccount(Long accountId){
-        Account account = accountRepo.findById(accountId).orElse(null);
+    //check customer
+    public boolean customerCheck(Long accountId){
+
+        Customer customer = customerRepo.findById(accountId).orElse(null);
+        return customer != null;
+    }
+
+    public boolean accountCheck(Long accountId){
+
+        Account account = getAccountByAccountId(accountId).orElse(null);
+        return account != null;
     }
 
     //get all accounts
