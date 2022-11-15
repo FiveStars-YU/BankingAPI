c6fe5bbaa1e1fc5b2e1f26536873f7781211ca2f
dupree719 <isaiahdup7@gmail.com>
2022-11-14 22:17:08 -0500

    not sure what this is doing

--- src/main/java/com/FiveStarsYU/BankingAPI/controllers/DepositController.java
+++ src/main/java/com/FiveStarsYU/BankingAPI/controllers/DepositController.java
@@ -1,42 +1,4 @@
-<<<<<<< HEAD
-//package com.FiveStarsYU.BankingAPI.controllers;
-//
-//import com.FiveStarsYU.BankingAPI.models.Customer;
-//import com.FiveStarsYU.BankingAPI.models.Deposit;
-//import com.FiveStarsYU.BankingAPI.repository.DepositRepo;
-//import com.FiveStarsYU.BankingAPI.services.DepositService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//@RestController
-//public class DepositController {
-//
-//    @Autowired
-//    private DepositRepo depositRepo;
-//    @Autowired
-//    private DepositService depositService;
-//
-//   // @PostMapping("/deposit/{customerId}/deposit")
-//   // public void createDeposit(@PathVariable Long customerId, @RequestBody Customer customer){
-//     //   depositService.createDeposit(accountId,deposit);
-//  //  }
-//    @GetMapping("/deposit/{accountId}/deposit")
-//    public Iterable<Deposit> getAllDepositsByAccountId(@PathVariable Long accountId){
-//        return depositService.getAllDepositsByAccountId(accountId);
-//    }
-//    @GetMapping("/deposit/{depositId}")
-//    public ResponseEntity<?> getDepositById(@PathVariable Long depositId){
-//        return depositService.getDepositByDepositId(depositId);
-//    }
-//    @DeleteMapping("/deposit/{depositId}")
-//    public void deleteDepositById(@PathVariable Long depositId){
-//        depositService.deleteDepositById(depositId);
-//    }
-//}
-=======
 package com.FiveStarsYU.BankingAPI.controllers;
-
 import com.FiveStarsYU.BankingAPI.errorhandling.CodeData;
 import com.FiveStarsYU.BankingAPI.errorhandling.CodeMessage;
 import com.FiveStarsYU.BankingAPI.errorhandling.CodeMessageData;
@@ -123,4 +85,4 @@ public class DepositController {
         }
     }
 }
->>>>>>> d31567201c840823ad8a25c610fe28c0b62c16b8
+
--- src/main/java/com/FiveStarsYU/BankingAPI/services/DepositService.java
+++ src/main/java/com/FiveStarsYU/BankingAPI/services/DepositService.java
@@ -1,48 +1,3 @@
-<<<<<<< HEAD
-//package com.FiveStarsYU.BankingAPI.services;
-//
-//import com.FiveStarsYU.BankingAPI.models.Account;
-//import com.FiveStarsYU.BankingAPI.models.Deposit;
-//import com.FiveStarsYU.BankingAPI.repository.AccountRepo;
-//import com.FiveStarsYU.BankingAPI.repository.DepositRepo;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.Optional;
-//
-//@Service
-//public class DepositService {
-//    @Autowired
-//    private DepositRepo depositRepo;
-//
-//  //  @Autowired
-//   // private AccountServices accountServices;
-//
-//    @Autowired
-//    private AccountRepo accountRepo;
-//
-//   // public Deposit createDeposit(Deposit deposit, Long accountId){
-//    //    accountServices.getAccountByAccountId()
-//   // }
-//    public Iterable<Deposit> getAllDepositsByAccountId(Long accountId){
-//        return depositRepo.getAllDepositByAccountId(accountId);
-//    }
-//    public ResponseEntity<?> getDepositByDepositId(Long depositId){
-//        Deposit deposit = depositRepo.findById(depositId).orElse(null);
-//        return new ResponseEntity<>(deposit, HttpStatus.OK);
-//    }
-//    public void deleteDepositById(Long depositId){
-//        depositRepo.deleteById(depositId);
-//    }
-//    public void updateDeposit(Long depositId, Deposit deposit){
-//        depositRepo.findById(depositId).map(account ->{
-//
-//       })
-//    }
-//}
-=======
 package com.FiveStarsYU.BankingAPI.services;
 
 import com.FiveStarsYU.BankingAPI.models.Account;
@@ -105,4 +60,4 @@ public class DepositService {
         return account != null;
     }
 }
->>>>>>> d31567201c840823ad8a25c610fe28c0b62c16b8
+
