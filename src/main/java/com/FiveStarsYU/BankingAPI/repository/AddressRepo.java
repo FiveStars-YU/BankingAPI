1dd69b58b6afa87e78dc0a6d4db056cff0ba7097
JazminRondon <Jazmin_Rondon@outlook.com>
2022-11-14 20:46:18 -0500

    commit customer classes

---
+++ src/main/java/com/FiveStarsYU/BankingAPI/repository/AddressRepo.java
@@ -0,0 +1,12 @@
+//package com.FiveStarsYU.BankingAPI.repository;
+//
+//import com.FiveStarsYU.BankingAPI.models.Address;
+//import org.springframework.data.repository.CrudRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface AddressRepo extends CrudRepository<Address,Long> {
+//
+//    Iterable<Address> findByCustomerId(Long customerId);
+//
+//}
