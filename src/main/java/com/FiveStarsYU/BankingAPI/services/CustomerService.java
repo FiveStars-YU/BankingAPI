96887519cb14f0604956a79059b45f32846128b7
JazminRondon <Jazmin_Rondon@outlook.com>
2022-11-16 09:41:05 -0500

    finished

--- src/main/java/com/FiveStarsYU/BankingAPI/services/CustomerService.java
+++ src/main/java/com/FiveStarsYU/BankingAPI/services/CustomerService.java
@@ -1,64 +1,59 @@
 package com.FiveStarsYU.BankingAPI.services;
 
 
-import com.FiveStarsYU.BankingAPI.models.Account;
-import com.FiveStarsYU.BankingAPI.models.Address;
 import com.FiveStarsYU.BankingAPI.models.Customer;
 import com.FiveStarsYU.BankingAPI.repository.CustomerRepo;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class CustomerService {
 
 
-
-
     @Autowired
-    private CustomerRepo customerRepo;
-
+    private CustomerRepo customerRepository;
     @Autowired
-    private AccountServices accountServices;
+    AccountServices accountServices;
 
+    public Customer createCustomer(Customer customer) {
 
-    public void addCustomer(Customer customer){
-
-        customerRepo.save(customer);
+        return customerRepository.save(customer);
     }
 
-    public Iterable<Customer> getAllCustomer(){
-        return customerRepo.findAll();
+    public List<Customer> getAllCustomers() {
+
+        List<Customer> listOfCustomers = new ArrayList<>();
+        customerRepository.findAll().forEach(listOfCustomers::add);
+        return listOfCustomers;
     }
 
-    public Optional<Customer> getCustomerById(Long id){
+    public Optional<Customer> getCustomerByAccountId(Long account_id) {
 
-        return customerRepo.findById(id);
+        Long customerId = accountServices.getAccountByAccountId(account_id).get().getId();
+        return customerRepository.findById(customerId);
     }
 
-    public Optional<Customer> getCustomerByAccountId(Long accountId){
-        Long customerId= accountServices.getAccountByAccountId(accountId).get().getId();
-        return customerRepo.findById(customerId);
+    public Optional<Customer> getCustomerByCustomerId(Long id) {
+        return customerRepository.findById(id);
     }
 
-    public void updateCustomer(Customer customer, Long id) {
-        customerRepo.save(customer);
+    public boolean customerCheck(Long customerId) {
+        Customer customer = customerRepository.findById(customerId).orElse(null);
+        return customer != null;
     }
+    public void updateCustomer(Customer customer) {
 
-    public void deleteCustomerById(Long id) {
-
-        customerRepo.deleteById(id);
+        customerRepository.save(customer);
     }
-
-    public boolean checkCustomerById(Long customerId){
-        Customer c= customerRepo.findById(customerId).orElse(null);
-        return c != null;
-    }
-
 
 }
+
+
+
+
+
+
